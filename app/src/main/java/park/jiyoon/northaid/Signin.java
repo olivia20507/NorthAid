866c5d4d7d2cd70cb4efee671c96f3daad4b8e13
olivia20507 <email@email.com>
2023-03-23 20:56:25 +0900

    hi

--- app/src/main/java/park/jiyoon/northaid/Signin.java
+++ app/src/main/java/park/jiyoon/northaid/Signin.java
@@ -1,7 +1,9 @@
 package park.jiyoon.northaid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +13,12 @@ public class Signin extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
+        Button Login = findViewById(R.id.signin);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(Signin.this, Mainpage.class);
+            }
+        });
     }
 }
